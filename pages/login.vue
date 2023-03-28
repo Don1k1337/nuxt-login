bf0ac54ed2b0a06c382de3edef30eacb12144f0a
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-03-28 18:02:42 +0600

    fix: cleaned up.

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,11 @@
 <template>
-  <login-form />
+  <login />
 </template>
 
 <script>
-import LoginForm from "../components/Forms/Login/LoginForm.vue";
 
 export default {
   name: "login",
-  components: {LoginForm}
 }
 </script>
 
